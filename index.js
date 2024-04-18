3f76f3456587415d64f9adb5a2090c4c49bd3d04
Alcaraz Nicolas <nicolasalcaraz18@gmail.com>
2024-04-18 18:27:10 -0300

    clase1, git config, instalacion de node y variable

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("buenos")
+console.log("Funciona piola por ahora, proto tendre quilmbos")
+/* el problema de usar las variables "var" es que se reasignan: 
+var nombre="ppp"
+nombre = "camilo"
+var nombre="kkkk"
+--------------------------------------------------------------------------------
+se puede invocar a una variable antes de declararla (que lenguaje de mierda)
+console.log(nombre)
+var nombre
+En fin este tipo de variables ya no hay que utilizarlas. Por compatibilidad con los navegadores sigue 
+estando vigente.
+---------------------------------------------------------------------------------
+el uso de let no permite redeclarar una variable
+let apellido 
+console.log(apellido)
+---------------------------------------------------------------------------------
+constantes
+const: siempre tiene que estar inicializada. una vez que se declara con un valor no se le puede
+reasignar un valor Ejemplo: 
+const edad=37
+edad=39
+tampoco se puede redeclarar
+const edad=45
+tiene que ser si o si con otro nombre.
+ */
