d45c7e5be5b08203fb456d2355f962f8989da557
Alcaraz Nicolas <nicolasalcaraz18@gmail.com>
2024-04-19 19:24:27 -0300

    clase2 primera parte

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
-console.log("buenos")
-console.log("Funciona piola por ahora, proto tendre quilmbos")
-/* el problema de usar las variables "var" es que se reasignan: 
-var nombre="ppp"
-nombre = "camilo"
-var nombre="kkkk"
---------------------------------------------------------------------------------
-se puede invocar a una variable antes de declararla (que lenguaje de mierda)
-console.log(nombre)
-var nombre
-En fin este tipo de variables ya no hay que utilizarlas. Por compatibilidad con los navegadores sigue 
-estando vigente.
----------------------------------------------------------------------------------
-el uso de let no permite redeclarar una variable
-let apellido 
-console.log(apellido)
----------------------------------------------------------------------------------
-constantes
-const: siempre tiene que estar inicializada. una vez que se declara con un valor no se le puede
-reasignar un valor Ejemplo: 
-const edad=37
-edad=39
-tampoco se puede redeclarar
-const edad=45
-tiene que ser si o si con otro nombre.
- */
+/*
+tipos de datos en java script.
+datos primitivos:
+string, numbers, boolean, null y undefined, bigint, symbol
+
+let str ="cadena";
+console.log(typeof str)
+
+let bool= false
+console.log(typeof bool)
+
+let num=123121414142443412412n // poniendole una n queda como un gran integer
+console.log(typeof num)
+
+let und =undefined // cuando no tiene ningun valor es indefinido.
+console.log(typeof und) 
+
+let sym=Symbol(23) //no se suele utilizar, genera datos unicos e irrepetibles
+console.log(typeof sym)
+
+let nl = null
+console.log(typeof nl) //tipo de dato que pronto tendrá un valor
+
+*/
+
+/*
+datos complejos/objetos
+
+const obj={
+    nombre:"camilo", // puede guardar coleccion de datos las cuales se pueden llamar a su key (obj)
+    apellido:"ojeda",
+}
+
+console.log(typeof obj) // objeto
+
+const arr=[1,"osval",{}, obj] //array que se maneja con indices, arranca con el indice 0 como en java.
+console.log(typeof arr)// guarda distintos tipos de datos, a diferencia del array en java.
+console.log(arr[3])
+
+const date = Date()
+console.log(typeof date) // a diferencia de crear una instancia(que devuelve un objeto), este me devuelve un string
+console.log(date) // devuelve la hora exacta
+
+*/
+/*
+    funciones.
+    sueltas son funciones, pero si están dentro de un objeto pasan a ser metodos
+
+function saludar(){ 
+    console.log("dibu martinez")
+}
+saludar(),// invocando el metodo
+console.log(typeof saludar) // es de tipo function
+*/
+/*
+arrow funtion(funciones flechas)
+forma simplificada de escribir funciones en una sola linea
+tienen un return implicito.
+*/
+const suma=(x,y)=> x+y; // se puede declarar con llaves, como si fuese un metodo
+console.log(typeof suma)
+console.log(suma(5,4))
+// otra manera de escribir
+const suma2 = (x,y) =>{
+    return x+y;
+};
+
