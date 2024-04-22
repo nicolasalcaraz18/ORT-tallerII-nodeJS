5a79341fd5057ed3660692fb63743c12c644997d
Alcaraz Nicolas <nicolasalcaraz18@gmail.com>
2024-04-22 10:45:30 -0300

    clase2 segunda parte

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ console.log(typeof saludar) // es de tipo function
 arrow funtion(funciones flechas)
 forma simplificada de escribir funciones en una sola linea
 tienen un return implicito.
-*/
+
 const suma=(x,y)=> x+y; // se puede declarar con llaves, como si fuese un metodo
 console.log(typeof suma)
 console.log(suma(5,4))
@@ -64,4 +64,84 @@ console.log(suma(5,4))
 const suma2 = (x,y) =>{
     return x+y;
 };
+*/
+/*
+    datos complejos
+    detro de data se guarda informacion.
+En el uso de datos primitivos no se ve afectada, esto sucede con los objetos.
+let nik="pablo";
+let nommbre = nik;
+console.log(nik);
+nombre="franco Roman"
+console.log(nombre);
+console.log(nik);
+*/
+
+/*
+ahora con objetos
+const data={
+    nombre:"juanito",
+    edad:30,
+}
+//prueba de datos complejos
+const userData=data
+console.log(data)
+console.log(userData)
+
+//en la linea 84 se pasa toda la estructura, por ende
+//termina manupulando toda la referencia (termina manipulando toda la informacion) 
+userData.nombre="Raul";
+console.log(data)
+console.log(userData)
+*/
+// Forma de copiar de objetos.
+/*
+const data={
+    nombre:"juanito",
+    edad:30,
+    titulos:["primario","secundario"],
+}
+const dataCopy={
+    nombre:data.nombre
+}
+*/
+/*
+dataCopy.nombre="Raul"
+console.log(data)
+console.log(dataCopy)
+*/
+
+/* metodo para realizar copias(spread copy)
+    forma rapida de copiar. creo que intermante tiene un array o 
+    alguna estrucutra repetitiva que se encarga de copiar la informacion original.
+    
+const dataSpread={...data}
+dataSpread.nombre="Ramiro"
+dataSpread.titulos.push("bachillerato")
+console.log(data)
+console.log(dataSpread)
+Object.assign es otro metodo para hacer copias. se utiliza de una manera un poco distinta
+pero su funcionalidad es la misma.
+*/
+/*
+    FORMAS DE COPYAS MAS PROFUNDAS
+    con json
+*/
+const data={
+    nombre:"juanito",
+    edad:30,
+    titulos:["primario","secundario"],
+}
+/*
+const jsonCopy=JSON.stringify(data)// es un formato json, supogo que el objeto tambien pasa a ese formato.
+console.log(typeof jsonCopy) // transforma todo el objeto a string
+*/
+// vuelvo a transormar el este jsonCopy a objeto
+// reconstruir el objeto
+// copia mas profunda, a juanito no le agrega el bachillerato.
+const jsonCopy=JSON.parse(JSON.stringify(data))
+console.log(typeof jsonCopy)
+jsonCopy.titulos.push("bachillerato")
+console.log(jsonCopy)
+console.log(data)
 
