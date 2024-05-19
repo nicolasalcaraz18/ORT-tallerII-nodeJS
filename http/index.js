079c6ab41eebe81cc9d4927d4a628dad6be52775
Nicolas <nicolasalcaraz18@gmail.com>
2024-05-19 16:55:33 -0300

    primera parte

---
+++ http/index.js
@@ -0,0 +1,10 @@
+import http from "node:http"
+console.log(http)
+
+/*
+Get: pedir información
+Post: enviar información. (enviar comentarios)
+Put: editar información. (editar un comentario por ejemplo de Facebook).
+Patch: editar información. (solo se edita un atributo)
+Delete: borrar un ítem (posteo)
+*/
