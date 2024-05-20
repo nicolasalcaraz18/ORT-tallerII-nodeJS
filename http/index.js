98bf4b547f2c147112132f6d4005d97e859dc1eb
Nicolas <nicolasalcaraz18@gmail.com>
2024-05-19 22:40:48 -0300

    http

--- http/index.js
+++ http/index.js
@@ -1,6 +1,4 @@
 import http from "node:http"
-console.log(http)
-
 /*
 Get: pedir información
 Post: enviar información. (enviar comentarios)
@@ -8,3 +6,15 @@ Put: editar información. (editar un comentario por ejemplo de Facebook).
 Patch: editar información. (solo se edita un atributo)
 Delete: borrar un ítem (posteo)
 */
+
+// crea el servidor y lo pone a la escucha
+// request = peticion; responsive == respuesta
+const app = http.createServer((req,res)=>{
+    //console.log(req)
+    res.end("hola_desde el servidor")
+})
+
+// pone a la escucha. Puerto 8080
+app.listen(8080,()=>{
+    console.log("server_ok")
+})
